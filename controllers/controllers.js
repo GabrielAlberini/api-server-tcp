1647a0acc279636c75433c7b8fbc850c79d5ca9c
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2023-11-14 21:48:47 -0300

    Incorporación de lógica en controladores

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,10 +1,40 @@
+import { readData, writeData } from "../utils/utils.js";
+
 // getters -> funciones que toman datos de la db
-// setters -> funciones que modifican la db
+const getPizzas = () => {
+  const pizzas = readData();
+  return pizzas;
+};
 
-const getPizzas = () => {};
+const getPizzaById = (id) => {
+  const pizzas = readData();
+  const pizza = pizzas.find((pizza) => pizza.id === id);
+  return pizza;
+};
 
-const getPizzaById = () => {};
+// setters -> fun ciones que modifican la db
+const addPizza = (nuevaPizza) => {
+  const pizzas = readData();
+  const pizza = pizzas.find((pizza) => pizza.nombre === nuevaPizza.nombre);
+  if (pizza) {
+    return "Ya existe la pizza :(";
+  }
+  pizzas.push(nuevaPizza);
+  writeData(JSON.stringify(pizzas));
+  return "Pizza agregada con éxito :)";
+};
 
-const addPizza = () => {};
+const deletePizzaById = (id) => {
+  const pizzas = readData();
+  const pizza = pizzas.find((pizza) => pizza.id === id);
+  if (pizza) {
+    const nuevasPizzas = pizzas.filter((pizza) => pizza.id !== id);
+    writeData(JSON.stringify(nuevasPizzas));
+    return "Pizza borrada con exito o.o";
+  }
+  return "La pizza no existe :O";
+};
 
-const deletePizzaById = () => {};
+console.log(deletePizzaById(2));
+
+export { getPizzas, getPizzaById, addPizza, deletePizzaById };
